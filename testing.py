795a9dd524df988e0d0f07d98b28bb2cbd8169dc
Idan Kideckel <idanbkideckel@gmail.com>
2023-01-18 11:56:06 +0200

    update

--- testing.py
+++ testing.py
@@ -2,52 +2,69 @@ from yolov8.ultralytics import YOLO
 import cv2
 import numpy as np
 
-def predict_pool(image):
-    model = YOLO("pool.pt")
-    outputs = model.predict(source=image, return_outputs=True) # treat predict as a Python generator
-    return add_layers_to_image(outputs, image)
+def predict_pool(source):
+    model = YOLO("best.pt")
+    outputs = model.predict(source=source, return_outputs=True) # treat predict as a Python generator
+    return outputs
+
+def predict_person(source):
+    model = YOLO("yolov8m.pt")
+    outputs = model.predict(source=source, return_outputs=True) # treat predict as a Python generator
+    return outputs
+
       
 
-def add_layers_to_image(outputs, img):
-    for output in outputs:
-        print("i am ",output," now bow")
-        if output == {}:
-            continue
-        # for detection
+def add_layers_to_image(output, img):
+    if not "det" in output:
+        return img
+
+    # for detection
+    if not "segment" in output:    
         for x in output["det"]:
             x1 , y1, x2, y2, score, label = x
             if label != 0:
                 continue
-            cv2.rectangle(img,(int(x1), int(y1)), (int(x2), int(y2)), color=0, thickness=2, lineType=cv2.LINE_AA)
-        # for segmentation
+            cv2.rectangle(img,(int(x1), int(y1)), (int(x2), int(y2)), color=(100,100,100), thickness=5, lineType=cv2.LINE_AA)   
+    # for segmentation
+    else:
+        if not "segment" in output:
+            return img
         for x in output["segment"]:
             x = np.matrix.round(x)
-            cv2.fillPoly(img, pts = np.int32([x]), color =(0,100,0))
+            cv2.fillPoly(img, pts = np.int32([x]), color =(0,100,0,0.5))
 
     return img
 
 
 
-def onVideo(source):
-    cap = cv2.VideoCapture(source)
+def onVideo(pools, people, source):
+    if source == "0":
+        cap = cv2.VideoCapture(int(source))
+    else:
+        cap = cv2.VideoCapture(source)
     if cap.isOpened() == False:
         print("error on opening file....")
         return
     
     (success, image) = cap.read()
-    while success:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image_display = predict_pool(image)
+    for pool, person in zip(pools, people):
+        if not success:
+            break
+        
+        image_display = add_layers_to_image(pool, image)
+        image_display = add_layers_to_image(person, image)
         cv2.imshow("Result", image_display)
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
 
-        (success, image) = cap.read()    
+        (success, image) = cap.read()   
     cv2.destroyAllWindows()
 
 
 
 if __name__ == "__main__":
-    onVideo("vidtest.mp4")
+    pool = predict_pool("vidtest.mp4")
+    person = predict_person("vidtest.mp4")
+    onVideo(pool, person, "vidtest.mp4")
