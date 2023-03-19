7affa942419b53673e47c2d2d511220f2507245a
ChenMoskovitz <chen.mosko.cm@gmail.com>
2023-03-19 17:37:31 +0200

    n

--- icCam/pages/Notifications/Notifications.jsx
+++ icCam/pages/Notifications/Notifications.jsx
@@ -3,15 +3,36 @@ import { useState, useEffect, useRef } from 'react';
 import registerNNPushToken from 'native-notify';
 // import * as Device from 'expo-device';
 // import * as notification from 'expo-notifications';
+// import React, { useState, useEffect } from 'react';
+import { getPushDataObject } from 'native-notify';
+import { Notifications } from 'expo';
 
 const MyNotifications = () => {
   registerNNPushToken(6828, 'ZbDvAG9OgKWW3jVtqio59y');
+
+  let pushDataObject = getPushDataObject();
+
+  useEffect(() => {
+    console.log(pushDataObject);
+}, [pushDataObject]);
+
+const sendNotification = async () => {
+  const content = {
+    title: 'Notification Title',
+    body: 'Notification body text',
+    data: { data: 'goes here' },
+  };
+  const trigger = { seconds: 2 };
+  await Notifications.scheduleNotificationAsync({ content, trigger });
+};
+
   return (
     <View>
      <TouchableOpacity style = {styles.button}>
-                  <Text style={styles.buttonText}>Press to Send Notification</Text>
-                  </TouchableOpacity>
-    </View>
+        <Text style={styles.buttonText}>Press to Send Notification</Text>
+     </TouchableOpacity>
+     <Button title="Send Notification" onPress={sendNotification} />
+</View>
   );
 }
 export default MyNotifications
