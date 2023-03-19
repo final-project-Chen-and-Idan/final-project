232ee7942e01170de5d4da43523713f0ed2b2919
ChenMoskovitz <chen.mosko.cm@gmail.com>
2023-03-19 15:38:27 +0200

    n

--- icCam/pages/Notifications/Notifications.jsx
+++ icCam/pages/Notifications/Notifications.jsx
@@ -1,24 +1,31 @@
 import { StyleSheet, Text, TouchableOpacity, View, Button, Platform } from 'react-native'
 import { useState, useEffect, useRef } from 'react';
 import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
+import * as notification from 'expo-notifications';
 
 const MyNotifications = () => {
-    return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
-        <Text>Your expo push token: {expoPushToken}</Text>
-        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Title: {notification && notification.request.content.title} </Text>
-          <Text>Body: {notification && notification.request.content.body}</Text>
-          <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-        </View>
-        <Button
-          title="Press to Send Notification"
-          onPress={async () => {
-            await sendPushNotification(expoPushToken);
-          }}
-        />
-      </View>
-    )
+ 
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+      {/* <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View> */}
+      <Button
+        title="Press to Send Notification"
+        onPress={async () => {
+          await sendPushNotification(expoPushToken);
+        }}
+      />
+    </View>
+  );
 }
 export default MyNotifications
