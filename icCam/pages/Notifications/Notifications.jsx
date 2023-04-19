dd602e3ef35f72be683eb10e400ddbe44241b2d7
Idan Kideckel <idanbkideckel@gmail.com>
2023-04-19 21:09:00 +0300

    sounds

--- icCam/pages/Notifications/Notifications.jsx
+++ icCam/pages/Notifications/Notifications.jsx
@@ -7,26 +7,10 @@ import registerNNPushToken from 'native-notify';
 import { getPushDataObject } from 'native-notify';
 import { Notifications } from 'expo';
 import PushNotification from 'react-native-push-notification';
-// import AlarmClock from 'react-native-alarm-clock';
-import Alarm from 'react-native-alarm';
-// import Alarm from 'react-native-alarm';
-import Sound from 'react-native-sound';
 import { Audio } from 'expo-av';
 
-
 const MyNotifications = () => {
-
-
-  // const setAlarm = () => {
-  //   AlarmClock.createAlarm({
-  //     hour: 6,
-  //     minute: 30,
-  //     message: 'Wake up!',
-  //   });
-  // };
-
-
-
+  const [sound, setSound] = useState()
 //   registerNNPushToken(6828, 'ZbDvAG9OgKWW3jVtqio59y');
 
 //   let pushDataObject = getPushDataObject();
@@ -35,22 +19,23 @@ const MyNotifications = () => {
 //     console.log(pushDataObject);
 // }, [pushDataObject]);
 
-const sendNotification = async () => {
-  // PushNotification.localNotification({
-  //   title: "title",
-  //   message: "remoteMessage.notification.body",
-  //   //ios and android properties
-  //   playSound: true,
-  //   soundName: 'sound.mp3',
-  //   //android only properties
-  //   channelId: 'your-channel-id',
-  //   autoCancel: true,
-  //   bigText: 'Face2Face: Beacon Timer Expired',
-  //   subText: 'Perhaps set your beacon timer for another hour?',
-  //   vibrate: true,
-  //   vibration: 300,
-  //   priority: 'high',
-  // })
+// const sendNotification = async () => {
+  
+//   PushNotification.localNotification({
+//     title: "title",
+//     message: "remoteMessage.notification.body",
+//     //ios and android properties
+//     playSound: true,
+//     soundName: 'sound.mp3',
+//     //android only properties
+//     channelId: 'your-channel-id',
+//     autoCancel: true,
+//     bigText: 'Face2Face: Beacon Timer Expired',
+//     subText: 'Perhaps set your beacon timer for another hour?',
+//     vibrate: true,
+//     vibration: 300,
+//     priority: 'high',
+//   })
 //   const content = {
 //     title: 'Notification Title',
 //     body: 'Notification body text',
@@ -58,40 +43,31 @@ const sendNotification = async () => {
 //   };
 //   const trigger = { seconds: 2 };
 //   await Notifications.scheduleNotificationAsync({ content, trigger });
-};
-const triggerAlarm = () => { 
-  // Alarm.createAlarm({
-  //   startAt: new Date(Date.now() + 10000), // Alarm will trigger 10 seconds from now
-  //   message: 'Alarm!',
-  //   soundName: 'default',
-  //   interval: 'daily',
-  // });
-};
-
-// const playSound = () => {
-//   const soundPath = '../../assets/alarm.wav'; 
-//   const sound = new Sound(soundPath, Sound.MAIN_BUNDLE, (error) => {
-//     if (error) {
-//       console.log('failed to load the sound', error);
-//       return;
-//     }
-//     sound.play(() => {
-//       sound.release();
-//     });
-//   });
 // };
 
+const playSound = async() => {
+  console.log('Loading Sound');
+  const { sound } = await Audio.Sound.createAsync( require('../../assets/alarm.mp3')
+  );
+  setSound(sound);
+
+  console.log('Playing Sound');
+  await sound.playAsync();
+}
+
+const stopSound = async ()=> {
+  sound.unloadAsync();
+}
 
   return (
     <View>
-      {/* <Button title="Set Alarm" onPress={setAlarm} /> */}
-     {/* <TouchableOpacity style = {styles.button}>
-        <Text style={styles.buttonText}>Press to Send Notification</Text>
-     </TouchableOpacity>
-     <Button title="Send Notification" onPress={sendNotification} /> */}
-     {/* <Button title="Trigger Alarm" onPress={triggerAlarm} /> */}
-     {/* <Button title="Play Sound" onPress={playSound} /> */}
-</View>
+  
+      <TouchableOpacity style = {styles.button}>
+          <Text style={styles.buttonText}>Press to Send Notification</Text>
+      </TouchableOpacity>
+      <Button title="play sound" onPress={playSound} />
+      <Button title="stop sound" onPress={stopSound} />
+    </View>
   );
 }
 export default MyNotifications
