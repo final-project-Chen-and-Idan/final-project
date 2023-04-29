1c9c69c3418efc05d29afb709fb207e64ae3b603
Idan Kideckel <idanbkideckel@gmail.com>
2023-04-29 16:31:32 +0300

    Update main.py

--- detection/main.py
+++ detection/main.py
@@ -1,12 +1,113 @@
+from cv2 import KeyPoint
+from zmq import NULL
 from ultralytics import YOLO
 import cv2
+import math
 import numpy as np
 import supervision as sv
 
+points = {"Nose":0,"Left Eye":1,"Right Eye":2,"Left Ear":3,"Right Ear":4,"Left Shoulder":5,"Right Shoulder":6,"Left Elbow":7,"Right Elbow":8
+                 ,"Left Wrist":9,"Right Wrist":10,"Left Hip":11,"Right Hip":12,"Left Knee":13,"Right Knee":14,"Left Ankle":15,"Right Ankle":16}
+
+# calculates the distance between 2 
+def calculate_keypoint_distance(keypoints, keypoint_a, keypoint_b):
+    if keypoints[points[keypoint_a]][2]>conf and keypoints[points[keypoint_b]][2]>conf:
+        return math.dist(keypoints[points[keypoint_a]][:2], keypoints[points[keypoint_b]][:2])
+    return -1
+
+# calculating the length of the torso
+def calculate_torso_length(keypoints):
+    
+    # calculating the distance between the left shoulder and left hip
+    lslh = calculate_keypoint_distance(keypoints,"Left Shoulder","Left Hip")
+   
+    #  calculating the distance between the right shoulder and right hip
+    rsrh = calculate_keypoint_distance(keypoints,"Right Shoulder","Right Hip")
+
+    # calculating the length of the torso
+    if rsrh!=-1 and lslh != -1:
+        torso_length = (lslh+rsrh)/2
+        return torso_length
+    else:
+        return -1
+
+# calculating the length of the limbs 
+def calculate_limb_length(keypoints):
+    # --------------- right arm ----------------------------
+    # calculating the distance between the right shoulder and right elbow
+    rsre = calculate_keypoint_distance(keypoints,"Right Shoulder","Right Elbow")
+    
+    # calculating the distance between the right elbow and right wrist
+    rerw = calculate_keypoint_distance(keypoints,"Right Elbow","Right Wrist")
+    
+    # calculating the right arm
+    if rsre != -1 and rerw != -1:
+        right_arm_length = rsre+rerw
+    else:
+        right_arm_length = -1
+    
+    # ----------------------- left arm ------------------------------
+    # calculating the distance between the left shoulder and left elbow
+    lsle = calculate_keypoint_distance(keypoints,"Left Shoulder","Left Elbow")
+    
+    # calculating the distance between the left elbow and left wrist
+    lelw = calculate_keypoint_distance(keypoints,"Left Elbow","Left Wrist")
+    
+    # calculating the left arm
+    if lsle != -1 and lelw != -1:
+        left_arm_length = lsle + lelw
+    else:
+        left_arm_length = -1
+        
+    # --------------- right leg ----------------------------
+    # calculating the distance between the right hip and right knee
+    rhrk = calculate_keypoint_distance(keypoints,"Right Hip","Right Knee")
+    
+    # calculating the distance between the right knee and right ankle
+    rkra = calculate_keypoint_distance(keypoints,"Right Knee","Right Ankle")
+    
+    # calculating the right arm
+    if rhrk != -1 and rkra != -1:
+        right_leg_length = rhrk+rkra
+    else:
+        right_leg_length = -1
+    
+    # ----------------------- left leg ------------------------------
+    # calculating the distance between the left hip and left knee
+    lhlk = calculate_keypoint_distance(keypoints,"Left Hip","Left Knee")
+    
+    # calculating the distance between the left knee and left ankle
+    lkla = calculate_keypoint_distance(keypoints,"Left Knee","Left Ankle")
+    
+    # calculating the left arm
+    if lhlk != -1 and lkla != -1:
+        left_leg_length = lhlk + lkla
+    else:
+        left_leg_length = -1
+        
+    return [right_arm_length, left_arm_length, right_leg_length, left_leg_length]
+
+# calculating the size of the head
+def calculate_head_size(keypoints):
+    pass
 
 def child_or_adult(img, box):
     cropped_image =  img[box[0]:box[1], box[2]:box[3],:]
-    cv2.imshow("cropped", cropped_image)
+    output = pose_model.predict(cropped_image)[0]
+    keypointsImage = output.plot()
+
+    # evey key point is in the format of [x,y, clf]
+    for out in output:
+        keypoints = out.keypoints.tolist()
+        torso = calculate_torso_length(keypoints)
+        limbs = calculate_limb_length(keypoints)
+        head = calculate_head_size(keypoints)
+        # for num, point in enumerate(out.keypoints.tolist()):
+        #     print(f"{points[num]}: x-{point[0]}. y-{point[1]}. clf={point[2]}")
+        #     if point[2]>0.5:
+        #         cv2.circle(cropped_image, [int(point[0]), int(point[1])], 5, (0, 0, 255), -1)
+    
+    cv2.imshow("cropped", keypointsImage)
     cv2.waitKey(1)
     pass
 
@@ -44,9 +145,6 @@ def predict(source, model, img):
 
 
 def onVideo(source):
-    model_pool = YOLO("poolm.pt")
-    model_person = YOLO("yolov8m.pt")
-    
     if source == "0":
         cap = cv2.VideoCapture(int(source))
     else:
@@ -55,9 +153,16 @@ def onVideo(source):
         print("error on opening file....")
         return
     
-    every_fith = 0
+    predict_every = 0
     (success, image) = cap.read()
     while success:
+        # predict every third frame to keep it in real time
+        if predict_every != 3:
+            predict_every += 1
+            (success, image) = cap.read()
+            continue
+        predict_every = 0
+        
         # getting the predictions of the pool and people and adding to image
         display_image = predict(image, model_pool, image)
         display_image = predict(image, model_person, display_image)
@@ -75,6 +180,14 @@ def onVideo(source):
 
 
 if __name__ == "__main__":
+    print("loading models.....")
+    pose_model = YOLO("yolov8m-pose.pt")
+    model_pool = YOLO("poolm.pt")
+    model_person = YOLO("yolov8m.pt")
+    print("finished loading models")
+    
+    conf = 0.5
+    
     box_annotator = sv.BoxAnnotator(
         thickness=2,
         text_thickness=2,
